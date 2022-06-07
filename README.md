c15b219ac27bccaa0e5064f1683cb0ed4e60552e
Mario Simão <mariosimao@poli.ufrj.br>
2022-06-06 23:21:04 -0300

    Add dev documentation

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# Poli Vagas Client
+# Poli Vagas WEB
+
+## Dev
+
+On your terminal execute:
+
+```bash
+$ npm i
+$ npm run dev
+```
+
+Navigate to [http://localhost:8080](http://localhost:8008) on your internet browser.
--- services/firebase.ts
+++ services/firebase.ts
@@ -13,8 +13,7 @@ export const initAuth = () => {
     onLogoutRequestError: (err) => {
       console.error(err)
     },
-    // Use application default credentials (takes precedence over firebaseAdminInitConfig if set)
-    // useFirebaseAdminDefaultCredential: true,
+    useFirebaseAdminDefaultCredential: true,
     firebaseClientInitConfig: {
       apiKey: "AIzaSyCWkBlR8bMBBm5QmqMSgsHixO0Foky6dTo",
       authDomain: "poli-vagas-auth.firebaseapp.com",
@@ -24,20 +23,14 @@ export const initAuth = () => {
       appId: "1:1070791957748:web:95ef2e09bc8dac696b12a1",
     },
     cookies: {
-      name: 'ExampleApp', // required
-      // Keys are required unless you set `signed` to `false`.
-      // The keys cannot be accessible on the client side.
-      keys: [
-        process.env.COOKIE_SECRET_CURRENT,
-        process.env.COOKIE_SECRET_PREVIOUS,
-      ],
+      name: 'PoliVagas', // required
       httpOnly: true,
       maxAge: 12 * 60 * 60 * 24 * 1000, // twelve days
       overwrite: true,
       path: '/',
       sameSite: 'strict',
       secure: true, // set this to false in local (non-HTTPS) development
-      signed: true,
+      signed: false,
     },
     onVerifyTokenError: (err) => {
       console.error(err)
