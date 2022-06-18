b532f8cefac52f82a85056868a8b1385678a22d0
Mario Simão <mariosimao@poli.ufrj.br>
2022-06-18 14:21:13 -0300

    Registration forms (#9)
    * Add company registration form
    
    * Add job registration form

--- services/company.ts
+++ services/company.ts
@@ -3,10 +3,27 @@ import axios from "axios";
 export type CompanyData = {
   id: string,
   name: string,
+  website: string|null,
+};
+
+export type CompanyInput = {
+  name: string,
+  website?: string
 };
 
 export const fetchCompanies = (): Promise<CompanyData[]> => {
   return axios
     .get('http://localhost:3000/companies')
     .then(response => response.data);
+}
+
+export const addCompany = (company: CompanyInput): Promise<string> => {
+  if (company.website?.length === 0) {
+    delete company.website;
+  }
+
+  return axios
+    .post('http://localhost:3000/companies', company)
+    .then(response => response.data.id)
+    .catch(error => { throw error.response.data.message; });
 }
