f71d0ffcdb1a265028f97952a9fa2b9c357dadb3
Mario Simão <mariosimao@poli.ufrj.br>
2022-06-19 12:09:39 -0300

    Change favicon (#17)
    * Add favicon
    
    * Remove dead assets

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,15 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import { initAuth } from '../services/firebase'
 import Navbar from '../components/Navbar';
+import Head from 'next/head';
 
 initAuth();
 
 const PoliVagas = ({ Component, pageProps }: AppProps) => {
   return (<>
+    <Head>
+      <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>💼</text></svg>"/>
+    </Head>
     <Navbar/>
     <Component {...pageProps} />
   </>)
